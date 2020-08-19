7c68cdd31064f947020058847361422708dee577
ThomasH94 <35856664+ThomasH94@users.noreply.github.com>
2020-08-19 14:49:35 -0700

    Initial Checkin
    Checking in the project to Github

---
+++ Assets/_Project/Scripts/Paddle System/PaddleController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrickBreak.Paddles
+{
+    public class PaddleController : MonoBehaviour
+    {
+        public PaddleData paddleData;
+        [SerializeField] private Rigidbody2D paddleRigidBody;
+        public bool usePhysics = true;
+
+        #region Input
+
+        private float _horizontalInput = 0.0f;
+
+        #endregion
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+            _horizontalInput = Input.GetAxisRaw("Horizontal"); // Read input in update so we don't miss any inputs
+            if(usePhysics)
+                return;
+            Vector2 moveVector = new Vector2(_horizontalInput * paddleData.moveSpeed, 0);
+            transform.Translate(moveVector.x * Time.deltaTime, moveVector.y, 0);
+        }
+
+        private void FixedUpdate()
+        {
+            if(!usePhysics)
+                return;
+            
+            Vector2 moveVector = new Vector2(_horizontalInput,0).normalized;
+            paddleRigidBody.MovePosition((Vector2) transform.position + moveVector * (paddleData.moveSpeed * Time.deltaTime));
+
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Paddle System/PaddleData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Paddle Data", fileName = "New Paddle Data")]
+public class PaddleData : ScriptableObject
+{
+    public float moveSpeed = 150.0f;
+    public Sprite paddleSprite;
+}
