23c9ef36b6f3d2bbf7df3fee0e9711176289f933
ThomasH94 <35856664+ThomasH94@users.noreply.github.com>
2020-08-19 23:42:19 -0700

    Paddle Screen Boundaries
    Added screen boundaries to the ball from the main camera controller script

---
+++ Assets/_Project/Scripts/Cameras/MainCameraController.cs
@@ -0,0 +1,40 @@
+﻿using BrickBreak.Singletons;
+using UnityEngine;
+
+namespace BrickBreak.Cameras
+{
+    public class MainCameraController : Singleton<MainCameraController>
+    {
+        public Camera mainCamera;
+
+        private float _horizontalScreenSize;
+        public float HorizontalScreenSize
+        {
+            get => _horizontalScreenSize;
+            set => _horizontalScreenSize = value;
+        }
+
+        private float _verticalScreenSize;
+        public float VerticalScreenSize
+        {
+            get => _verticalScreenSize;
+            set => _verticalScreenSize = value;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            mainCamera = Camera.main;
+            OnScreenSizeUpdated();
+        }
+
+        public void OnScreenSizeUpdated()
+        {
+            if (mainCamera != null)
+            {
+                _verticalScreenSize = mainCamera.orthographicSize;
+                _horizontalScreenSize = (_verticalScreenSize * Screen.width) / Screen.height;
+            }
+        }
+    }
+}
